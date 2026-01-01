6a4af21a645d3c10a3a3ae0cfe0509b7addf7b72
Seydina Thiam <seydinat235@gmail.com>
2026-01-01 13:34:22 +0100

    feat: Mise en et initialisation des differents dependances pour le developpement

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <p className="text-red-500">Bonjour react</p>
+      <Button className="hover:bg-blue-500 hover:cursor-pointer">Click me</Button>
     </div>
   );
 }
