cd7ef79e06cc634d0eca009a2446336a726834eb
Seydina Thiam <seydinat235@gmail.com>
2026-02-14 13:50:46 +0000

    feat: mise à jour de la gestion des rôles dans MemberCard et correction de la configuration de l'API

--- src/shared/services/api/config/api.config.js
+++ src/shared/services/api/config/api.config.js
@@ -3,7 +3,7 @@
  */
 export const API_CONFIG = {
   BASE_URL:
-    import.meta.env.VITE_API_BASE_URL || "https://perle-api.onrender.com/api",
+    import.meta.env.VITE_API_BASE_URL,
   TIMEOUT: 30000,
   RETRY_ATTEMPTS: 3,
   RETRY_DELAY: 1000,
